a662db738ff7a1eb786afcd5454033fd59e79e0e
st181891 <st181891@stud.uni-stuttgart.de>
2025-07-15 22:35:10 +0200

    Grundskelett

--- functions.py
+++ functions.py
@@ -1,11 +1,27 @@
+from decimal import Decimal, getcontext
+
+# Präzision der Rechnungen in Stellen festlegen
+getcontext().prec = 28
+
+# a und b können hier float oder int sein.
+# Decimal() stellt ein eigenes Zahlensystem bereit.
+# Beruht auf Basis Zehnerpotenz plus Mantisse.
 def add(a,b):
-    return a+b
+    da = Decimal(str(a))
+    db = Decimal(str(b))
+    return da + db
 
 def sub(a,b):
-    return a-b
+    da = Decimal(str(a))
+    db = Decimal(str(b))
+    return da - db
 
 def mul(a,b):
-    return a*b
+    da = Decimal(str(a))
+    db = Decimal(str(b))
+    return da * db
 
 def div(a,b):
-    return a/b
+    da = Decimal(str(a))
+    db = Decimal(str(b))
+    return da / db
--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 import functions
+#Wir benutzen hier decimal, um das Runden der Floats zu vermeiden.
+from decimal import Decimal
+# Dictionary, um Eingaben auf Funktionen zu mappen.
+ops = {
+    "add": functions.add,
+    "sub": functions.sub,
+    "mul": functions.mul,
+    "div": functions.div,
+}
+
 v = True
-print("Was berechnen wir heute?")
-print("Falls du das Programm während der Schleife beenden willst drück Strg + C")
+print("Was berechnen wir heute?\n"
+      "Falls du das Programm während der Schleife beenden willst "
+      "drück Strg + C.")
 while v:
     x = input("Du hast folgende Optionen: add, sub, mul, div.\n")
-    if x not in ("add", "sub", "mul", "div"):
+    if x not in ops:
         print("Falsche Eingabe.")
         continue
 
     while True:
+        y_str = input("Was soll die erste Zahl sein?\n")
         try:
-            y = float(input("Was soll die erste Zahl sein?\n"))
+            y = Decimal(y_str)
         except ValueError:
             print("Bitte nur Zahlen eingeben")
-            continue
         else:
             break
 
     while True:
+        z_str = input("Was soll die zweite Zahl sein?\n")
         try:
-            z = float(input("Was soll die zweite Zahl sein?\n"))
+            z = Decimal(z_str)
         except ValueError:
             print("Bitte nur Zahlen eingeben")
-            continue
         else:
             break
 
-    if x == "add":
-        print(functions.add(y, z))
-    elif x == "sub":
-        print(functions.sub(y, z))
-    elif x == "mul":
-        print(functions.mul(y, z))
-    elif x == "div":
-        print(functions.div(y, z))
+    # Sucht nach Schlüssel im dict,
+    # existiert er bekommen wir die Fkt.
+    # Alternativ liefert die Funktion None/False.
+    func = ops.get(x)
+    if func:
+        result = func(y,z)
+        print(result)
     else:
-        print("Wrong input")
+        print("Falscher input.")
 
     print("Willst du weiter rechnen? (y/n)")
     x = input()
